40e47119ff2429c3e19bbaf77bf676fa4a86b85c
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-13 17:00:02 -0400

    Image added to Hero component.

--- src/components/home-page/hero.tsx
+++ src/components/home-page/hero.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import classes from '../../styles/hero.module.css';
+import Image from 'next/image';
 
 const Hero: React.FC = () => {
   return (
-    <section>
-      <div>
-        Image
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/juan-miguel.jpg"
+          alt="Juan Miguel"
+          width={300}
+          height={300}
+        />
       </div>
       <h1>Hi, I&apos;m Juan Miguel</h1>
       <p>
