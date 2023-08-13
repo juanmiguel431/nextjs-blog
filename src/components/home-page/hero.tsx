27ad9839a05da4d44612eab0fbaedfcb35d8e30d
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-13 16:46:58 -0400

    Hero component added.

---
+++ src/components/home-page/hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero: React.FC = () => {
+  return (
+    <section>
+      <div>
+        Image
+      </div>
+      <h1>Hi, I&apos;m Juan Miguel</h1>
+      <p>
+        I blog about web development, specially frontend frameworks like Angular or React.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from 'next';
+import Hero from '@/components/home-page/hero';
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Home Page</h1>
+      <Hero/>
     </>
   )
 }
