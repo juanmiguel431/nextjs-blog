6cd1532c76f1d7c17b3bc4a0864e5b8e975d42b5
juanmiguel431 <juanmiguel431@gmail.com>
2023-08-16 22:33:35 -0400

    Mongo Db added.

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,11 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import MongoDbClient from '@/apis/mongodb';
 
 type Data = {
   message: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -23,8 +24,18 @@ export default function handler(
       return;
     }
 
-    const messageObj = { email, name, message };
+    const client: MongoDbClient = new MongoDbClient("messages");
+    try {
+      await client.connect();
+      const messageObj = { email, name, message };
+      const result = await client.insert(messageObj);
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong, please try again.' });
+      return
+    } finally {
+      await client.close();
+    }
 
-    res.status(201).json({ message: 'Successfully stored message.' })
+    res.status(201).json({ message: 'Successfully stored message.' });
   }
 }
